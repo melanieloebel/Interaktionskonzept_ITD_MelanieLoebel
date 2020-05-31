02f5639e97bb49ffb3df9d2ab1c83ab3a668735b
melanieloebel <melanie.loebel@stud.hshl.de>
2020-05-31 13:35:15 +0200

    update

--- Implementation/doctor.py
+++ Implementation/doctor.py
@@ -3,37 +3,42 @@ import json
 import time
 import datetime
 import appointment
-from datetime import *
-from appointment import *
+
 
 class Doctor():
 
     #Konstruktor
-    def __init__(self, title, name, specialist, phoneno): #, res_appointments):
+    def __init__(self, title, name, specialist, phoneno):
         self.title = title
         self.name = name
         self.specialist = specialist
         self.phoneno = phoneno
-        #self.res_appointments = res_appointments
-
+      
 
 doctors = []
-#res_appointments = []
 
 
 def save_appointment(self, patient_name, app_date):
-        self.patient_name = input("Please enter your name: ")
+        self.patient_name = patient_name
+        self.app_date = app_date
+
+        #Input name of patient
+        patient_name = input("Please enter your name: ")
+
+        #Input and parse of wish appointment
         request_date = datetime.strptime(input ("Please enter your wish appointment in format YYYY-MM-DD: "), "%Y-%m-%d")
-        
-        self.app_date = request.strftime("%d.%m.%Y")
+
+        #Format appointment
+        app_date = request_date.strftime("%d.%m.%Y")
         
         print (app_date)
+       
+        
 
 
-        #res_appointment.append(patient_name, date, time)
-
-doc1 = Doctor()
-doc1.save_appointment()
+        
+doc1 = Doctor('Dr.','Mel','Innere Medizin','123')
+doc1.save_appointment
 
 
 
--- Implementation/main.py
+++
@@ -1,4 +0,0 @@
-from doctor import *
-
-doctor_2 = Doctor("Dr.", "Klaus Topp", "Innere Medizin", "02941-111111")
-print (doctor_2.phoneno)
